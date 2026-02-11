db46f2e5df920383fe2277bf8665172c0613f2eb
alison <adevpr11@gmail.com>
2026-02-11 11:13:47 -0300

    feat: implementa consulta por nome do cliente

--- src/main/java/com/nca/api_clientes/controllers/ClienteController.java
+++ src/main/java/com/nca/api_clientes/controllers/ClienteController.java
@@ -3,10 +3,7 @@ package com.nca.api_clientes.controllers;
 import com.nca.api_clientes.dtos.ClienteRequest;
 import com.nca.api_clientes.repositories.ClienteRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
@@ -23,6 +20,16 @@ public class ClienteController {
         try {
             this.clienteRepository.salvar(clienteRequest);
             return ResponseEntity.status(201).body("Cliente cadastrado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<?> obterPorNome(@PathVariable String nome) {
+        try {
+            var clientes = this.clienteRepository.obterPorNome(nome);
+            return ResponseEntity.status(200).body(clientes);
         } catch (Exception e) {
             return ResponseEntity.status(500).body(e.getMessage());
         }
--- src/main/java/com/nca/api_clientes/repositories/ClienteRepository.java
+++ src/main/java/com/nca/api_clientes/repositories/ClienteRepository.java
@@ -1,17 +1,22 @@
 package com.nca.api_clientes.repositories;
 
 import com.nca.api_clientes.dtos.ClienteRequest;
+import com.nca.api_clientes.entities.Cliente;
 import com.nca.api_clientes.factories.ConnectionFactory;
 import org.springframework.stereotype.Repository;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class ClienteRepository  {
+public class ClienteRepository {
 
     private ConnectionFactory connectionFactory;
 
-    public ClienteRepository(ConnectionFactory connectionFactory){
+    public ClienteRepository(ConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
     }
 
@@ -34,6 +39,41 @@ public class ClienteRepository  {
 
         } catch (Exception e) {
             throw new RuntimeException("Erro ao inserir cliente", e);
+        }
+    }
+
+    public List<Cliente> obterPorNome(String nome) throws SQLException {
+
+        try (var connection = connectionFactory.getConnection()) {
+
+            var sql = """
+                    SELECT id, nome, email, telefone, datacadastro, ativo
+                        FROM clientes
+                     WHERE ativo = 1
+                       AND nome ILIKE ?""";
+
+            var statement = connection.prepareStatement(sql);
+
+            statement.setString(1, "%" + nome + "%");
+
+            var clientesResultSet = statement.executeQuery();
+
+            var clientes = new ArrayList<Cliente>();
+
+            while (clientesResultSet.next()) {
+                clientes.add(new Cliente(
+                        (UUID) clientesResultSet.getObject("id"),
+                        clientesResultSet.getString("nome"),
+                        clientesResultSet.getString("email"),
+                        clientesResultSet.getString("telefone"),
+                        new Date(clientesResultSet.getTimestamp("datacadastro").getTime()),
+                        true));
+            }
+
+            return clientes;
+
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao obter por nome: ", e);
         }
     }
 }
