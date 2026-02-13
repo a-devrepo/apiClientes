6325fe21c5a9787bca1aa7ccd2a603497b2b7760
alison <adevpr11@gmail.com>
2026-02-13 10:53:41 -0300

    feat(controller): adiciona endpoints put e delete

--- src/main/java/com/nca/api_clientes/controllers/ClienteController.java
+++ src/main/java/com/nca/api_clientes/controllers/ClienteController.java
@@ -1,9 +1,12 @@
 package com.nca.api_clientes.controllers;
 
 import com.nca.api_clientes.dtos.ClienteRequest;
+import com.nca.api_clientes.entities.Cliente;
 import com.nca.api_clientes.repositories.ClienteRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
@@ -30,6 +33,41 @@ public class ClienteController {
         try {
             var clientes = this.clienteRepository.obterPorNome(nome);
             return ResponseEntity.status(200).body(clientes);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody ClienteRequest cliente) {
+        try {
+            var clienteRegistrado = new Cliente();
+
+            clienteRegistrado.setId(id);
+            clienteRegistrado.setNome(cliente.nome());
+            clienteRegistrado.setEmail(cliente.email());
+            clienteRegistrado.setTelefone(cliente.telefone());
+
+            if (!this.clienteRepository.atualizar(clienteRegistrado)) {
+
+                return ResponseEntity.status(400).body("Cliente não encontrado.");
+            }
+
+            return ResponseEntity.status(200).body("Cliente atualizado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        try {
+            if (!this.clienteRepository.excluir(id)) {
+
+                return ResponseEntity.status(400).body("Cliente não encontrado.");
+            }
+
+            return ResponseEntity.status(200).body("Cliente excluído com sucesso.");
         } catch (Exception e) {
             return ResponseEntity.status(500).body(e.getMessage());
         }
